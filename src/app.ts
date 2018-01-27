e5401be339b8b70b88caf3c79b377eef1a735cd8
Giovanny Betancourt G <geobg711@hotmail.com>
2018-01-27 16:29:16 -0500

    ok

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+const pizzas = [{ name: 'Pepperoni', toppings: ['pepperoni'] }];
+
+const mappedPizzas = pizzas.map(pizza => pizza.name.toUpperCase());
+
+console.log(mappedPizzas);
+
+function createPizza () {
+
+}
+
+const pizza = {
+  name: 'Blazing Inferno',
+  getName: function() {
+    console.log(this.name);
+  }
+};
+
+console.log(pizza.getName());
