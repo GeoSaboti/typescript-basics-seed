69e451bf6a548945bf7d39dbc826f8330cafc634
Giovanny Betancourt G <geobg711@hotmail.com>
2018-01-29 18:00:09 -0500

    ok

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,29 @@
 abstract class Sizes {
-  constructor(public sizez: string[]){}
+  constructor(protected sizes: string[]){}
 
   set availableSizes(sizes: string[]) {
-    this.sizez = sizes;
+    this.sizes = sizes;
   }
 
   get availableSizes() {
-    return this.sizez;
+    return this.sizes;
   }
 }
 
 // Solo se puede instanciar mediante una extensión de la calse
-new Sizes(['small']);
+// new Sizes(['small']);
 
 // Solo se puede instanciar de esta manera
 class Pizza extends Sizes{
 
   public toppings: string[] = [];
 
-  constructor(readonly name: string, public sizes: string[]){
+  constructor(readonly name: string, sizes: string[]){
     super(sizes);
+  }
+
+  public updateSizes(sizes: string[]) {
+    this.sizes = sizes;
   }
 
   public addTopping (topping: string) {
@@ -31,6 +35,5 @@ class Pizza extends Sizes{
 
 const pizza = new Pizza('Pepperoni', ['small', 'medium']);
 console.log(pizza.availableSizes);
-pizza.addTopping('pepperoni');
-console.log(pizza.name);
-console.log(pizza);
+pizza.updateSizes(['large']);
+console.log(pizza.availableSizes);
