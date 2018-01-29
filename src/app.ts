40f9fb4bdd02052736e7fc09c565e08f262d7573
Giovanny Betancourt G <geobg711@hotmail.com>
2018-01-29 12:44:05 -0500

    ok

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,12 @@
-// function sumOrder (price: number, quantity: number): number {
-//   return price * quantity;
-// }
+let sumOrder2: (price: number, quantity?: number) => number;
 
-let sumOrder = (price: number, quantity: number): number => {
-  return price * quantity;
+sumOrder2 = (x, y) => {
+  if (y) {
+    return x * y;
+  }
+  return x;
 };
 
-const sum = sumOrder(12, 2);
-
-console.log(`Totalsum: ${sum}`);
-
-let sumOrder2: (price: number, quantity: number) => number;
-
-sumOrder2 = (x, y) => x * y;
-
-const sum2 = sumOrder2(25, 2);
+const sum2 = sumOrder2(25);
 
 console.log(`Totalsum: ${sum2}`);
