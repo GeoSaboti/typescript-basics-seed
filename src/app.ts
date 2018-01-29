698769d47adc197183f1f80746e110e8adf477b2
Giovanny Betancourt G <geobg711@hotmail.com>
2018-01-29 17:08:11 -0500

    ok

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ interface Pizza extends Sizes {
   name: string;
   toppings?: number;
   getAvailableSizez(): string[];
+  [key: number]: string;
 };
 
 let pizza: Pizza;
@@ -21,4 +22,5 @@ function createPizza(name: string, sizes: string[]): Pizza {
 }
 
 pizza = createPizza('Pepperoni', ['small', 'medium']);
+pizza[1] = 'xyz';
 pizza.toppings = 1;
