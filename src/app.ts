4a5e530c4012526dd82bd4c8c323369eaa836104
Giovanny Betancourt G <geobg711@hotmail.com>
2018-01-29 17:27:12 -0500

    ok

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,17 @@
-// ES5
-// function Pizza(name: string) {
-//   this.name = name;
-//   this.topings = [];
-// }
-
-// Pizza.prototype.addTopping = function addTopping(topping: string) {
-//   this.topings.push(topping);
-// };
-
-// const pizza = new Pizza('Peperoni');
-
-// pizza.addTopping('pepperroni')
-
-// TypeScript
-
 class Pizza {
 
-  name: string;
-  toppings: string[] = [];
+  public toppings: string[] = [];
 
-  constructor(name: string){}
+  constructor(private name: string){}
 
-  addTopping (topping: string) {
+  public addTopping (topping: string) {
     this.toppings.push(topping);
   }
 
 }
+
+const pizza = new Pizza('Pepperoni');
+
+pizza.addTopping('pepperoni');
+
+console.log(pizza);
